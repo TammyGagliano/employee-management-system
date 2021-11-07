9e6a2185fce0d4f8298a72e8bc530d35b1a04501
Tammy Gagliano <tracsd@gmail.com>
2021-11-07 10:46:44 -0600

    Begin Questionnarie on Index.js file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
-const db = require(".");
+const db = require("./db");
+const inquirer = require("inquirer");
 require("console.table");
 
 init();
@@ -11,3 +12,59 @@ function init() {
     console.log(logoText);
 }
 
+function empQuestions {
+    inquirer
+    .prompt ([
+        {
+            type: "list",
+            name: "options",
+            message: "What would you like to do?"
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "View all employees by manager",
+                "Add employee",
+                "Remove employee",
+                "Update employee by role",
+                "Update employee manager",
+                "exit"
+            ],
+            name: "choice"
+        }
+    ]).then(function(res){
+        console.log(res.choice);
+        switch (res.choice) {
+            case "View all employees":
+                employeeView();
+                break;
+            
+            case "View all employees by department":
+                departmentView();
+                break;
+          
+            case "View all employees by Manager":
+                managerView();
+                break;
+          
+            case "Add Employee":
+                employeeAdd();
+                break;
+          
+            case "Remove Employee":
+                employeeRemove();
+                break;
+          
+            case "Update Employee Role":
+                employeeUpdate();
+                break;
+          
+            case "Update Manager":
+                employeeManager();
+                break;
+          
+            case "Quit":
+                connection.end();
+                break;         
+        }
+    })
+};
