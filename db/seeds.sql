6ff68b7439fb6a357aa3e67962b91be8f220a947
Tammy Gagliano <tracsd@gmail.com>
2021-11-07 10:03:13 -0600

    Update seeds file to add more employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,23 @@
 USE company_db;
 
 -- insert data into department table
-INSERT INTO department (id, department)
+INSERT INTO department (name)
 VALUES ('Engineering', 'Software', 'Legal', 'Development');
 
 -- insert data into roles table
-INSERT INTO roles (id, title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
 ('Engineer', 125,000, 1),
 ('Front-end', 85,000, 2),
 ('Back-end', 110,000, 3),
 ('Tester', 75,000, 4),
+('Attorney', 150,000, 1),
+('Assistant', 45,000, 2),
+('Project Manager', 110,000, 3),
+('Developer Lead', 75,000, 4),
 
 -- insert data into the employee table
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
 ('Tim', 'Smith', 1, NULL),
 ('Gina', 'Erwin', 2, 1),
@@ -22,3 +26,4 @@ VALUES
 ('Tom', 'Collins', 5, NULL),
 ('Samantha','Darrin', 6, 2),
 ('Katherine','Young', 7, 1);
+('Michael', 'Dubous', 8, NULL)
