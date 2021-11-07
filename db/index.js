47626295f48f21f7bbcdaa279be702cf73c58725
Tammy Gagliano <tracsd@gmail.com>
2021-11-07 11:14:23 -0600

    updates to index files questions and queries

--- db/index.js
+++ db/index.js
@@ -1,14 +1,32 @@
 const connection = require('./connection');
 
-class  {
+class employeeDB {
 constructor(connection) {
-    this connection = connection;
+    this.connection = connection;
 }
 
-// Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
-findAllEmployees() {
-    
+// Show all employees
+allEmployees() {
+    return.this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;");
 }
+
+// Add an employee
+
+// Update a given employee's role
+
+// Show all managers
+
+// Show all roles
+
+
+// Create a new role 
+
+
+// Show all departments 
+
+// Add a department 
+
+module.exports = new employeeDB(connection);
 
 
 }
